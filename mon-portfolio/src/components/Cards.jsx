60184ed0450c4abb164f926bffa41714bc74b794
YoDaMned59 <sduvivier59112@gmail.com>
2025-03-03 17:09:17 +0100

    change style SCSS card an title "project"

--- mon-portfolio/src/components/Cards.jsx
+++ mon-portfolio/src/components/Cards.jsx
@@ -9,39 +9,41 @@ import "../styles/projects.scss";
 export const Cards = ({ data, openModal }) => {
     return (
         <Swiper
-            modules={[Navigation, Pagination]}
-            spaceBetween={20}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            loop={true}
-            breakpoints={{
-                768: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 }
-            }}
+        modules={[Navigation, Pagination]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        loopAdditionalSlides={1}
+        initialSlide={0}
+        breakpoints={{
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 }
+        }}
+            className="swiper-container"
         >
-            {data.length > 0 &&
-                data.map((project) => (
-                    <SwiperSlide key={project.id}>
-                        <h3>{project.title}</h3>
-                        <img
-                            src={project.image}
-                            alt={project.title}
-                            className="carousel-image"
-                        />
-                        <button onClick={() => openModal(project.id)} className="open-modal-btn">
-                            Description du projet
-                        </button>
-                        <div className="carousel-links">
-                            <a href={project.github} target="_blank" rel="noopener noreferrer">
-                                <FaGithub size={24} /> GitHub
-                            </a>
-                            <a href={project.site} target="_blank" rel="noopener noreferrer" className="site-link">
-                                <FaGlobe size={20} /> Voir le site
-                            </a>
-                        </div>
-                    </SwiperSlide>
-                ))}
+            {data.map((project) => (
+                <SwiperSlide key={project.id}>
+                    <h3>{project.title}</h3>
+                    <img
+                        src={project.image}
+                        alt={project.title}
+                        className="carousel-image"
+                    />
+                    <button onClick={() => openModal(project.id)} className="open-modal-btn">
+                        Description du projet
+                    </button>
+                    <div className="carousel-links">
+                        <a href={project.github} target="_blank" rel="noopener noreferrer">
+                            <FaGithub size={24} /> GitHub
+                        </a>
+                        <a href={project.site} target="_blank" rel="noopener noreferrer" className="site-link">
+                            <FaGlobe size={20} /> Voir le site
+                        </a>
+                    </div>
+                </SwiperSlide>
+            ))}
         </Swiper>
     );
 };
